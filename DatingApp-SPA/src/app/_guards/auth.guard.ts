157d38b9c7b2f31ab4c1ff02c36bdf5934361c55
Varun Chauhan <varun.chauhan@gmail.com>
2018-10-12 00:10:53 +0530

    End of section 7 - Added Routes and Guards

---
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private alertify: AlertifyService, private router: Router) {
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+      if (this.authService.loggedIn()) {
+        return true;
+      }
+      this.alertify.error('Unauthorized access!');
+      this.router.navigate(['/home']);
+      return false;
+  }
+}
